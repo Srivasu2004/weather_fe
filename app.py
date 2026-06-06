69ef48338b1ed39879a1ded41afa51e8a4e57da5
Your Name <srivasuramisetty@gmail.com>
2026-06-06 13:19:45 +0530

    app.py and requirements.txt succesfully

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+
+S_URL = "http://127.0.0.1:8000"
+
+st.title("🌤 AI Weather Agent")
+
+city = st.text_input("Enter City")
+
+question = st.text_input(
+    "Ask Your Weather Question"
+)
+
+if st.button("Ask Agent"):
+
+    try:
+
+        res = requests.post(
+            f"{S_URL}/get_weather",
+            params={
+                "city": city,
+                "question": question
+            }
+        )
+
+        if res.status_code == 200:
+
+            data = res.json()
+
+            st.success("Answer Generated")
+
+            st.write("### City")
+            st.write(data["city"])
+
+            st.write("### Question")
+            st.write(data["question"])
+
+            st.write("### Answer")
+            st.write(data["answer"])
+
+        else:
+            st.error(res.text)
+
+    except Exception as e:
+        st.error(str(e))
