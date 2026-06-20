d0d21bef0e1bbf4f3c78e2a5893e24a133c6fcd9
Ramisetty Sri vasu <srivasuramisetty@gmail.com>
2026-06-21 00:36:28 +0530

    Refactor weather data display logic

--- app.py
+++ app.py
@@ -72,15 +72,10 @@ if generate:
     # =========================
     st.subheader("🌦 Weather Overview")
     if "temperature" in data["weather"]:
-        st.success(
-        f"{data['weather']['weather']} | "
-        f"{data['weather']['temperature']}°C | "
-        f"Humidity {data['weather']['humidity']}%"
-    )
-else:
-    st.warning("Weather data not available")
-
-    st.divider()
+        st.success( f"{data['weather']['weather']} | " f"{data['weather']['temperature']}°C | " f"Humidity {data['weather']['humidity']}%" ) 
+    else:
+        st.warning("Weather data not available")
+        st.divider()
 
     # =========================
     # RECOMMENDATIONS
